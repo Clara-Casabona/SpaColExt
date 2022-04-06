4402c564d46897fd8f066dea73aa9c9c349c93b6
Clara-Casabona <claracasabona@gmail.com>
2022-04-06 10:19:32 -0400

    solow priors

--- R/prior_m.R
+++ R/prior_m.R
@@ -1,12 +1,4 @@
-#' Solow 1993 prior function for m
-#'
-#' @param m
-#'
-#' @return prior function for m
-#' @export
-#'
-#' @examples
-#' NOT APPLY
-dprior_m = function(m){
+
+solowdprior_m = function(m){
 1 / m
 }
--- R/prior_te.R
+++ R/prior_te.R
@@ -1,13 +1,4 @@
-#' Solow 1993 Prior function for te
-#'
-#' @param te
-#'
-#' @return Prior function for te
-#' @export
-#'
-#' @examples
-#' NOT APPLY
-#'
-dprior_te = function(te) {
+
+solowdprior_te = function(te) {
   1
   }
