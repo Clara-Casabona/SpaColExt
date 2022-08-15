137d4ac2cd9b040644cdf129fde78f8aa308b95e
Clara-Casabona <claracasabona@gmail.com>
2022-08-15 14:22:26 -0400

    Adding flexible prior

--- R/compute_posterior_c2022_extinction.R
+++ R/compute_posterior_c2022_extinction.R
@@ -1,41 +1,43 @@
-#' Compute Posterior extant probability with new TE prior
+#' Compute  Posterior Extinction with informative priors
 #'
-#' @param sightings matrix array with years by cell
-#' @param start_year  Start of the study period
-#' @param stop_year   End of the study period
-#' @param extinction_posteriorLevel Posterior extant probability of a higher spatial level analysis
+#' @param sightings list of observations
+#' @param start_year first year of study period
+#' @param stop_year  last year of study period
+#' @param prior_te   flexible function - add prior extinction rate distribution
+#' @param prior_m    flexible function - add prior observation rate distribution
 #'
-#' @return the posterior extrant probability of an species by location
+#' @return
 #' @export
 #'
 #' @examples
-#'
-compute_posterior_c2022_extinction = function(sightings,start_year, stop_year, extinction_posteriorLevel) {
-  apply(
-    sightings,
-    c(1, 2),
-    function(s) {
-      s = unlist(s)
 
-      if( any(is.na(s)) ) return(NA)
+compute_posterior_c2022_extinction <- function(sightings, start_year, stop_year, prior_te = NULL, prior_m = NULL) {
 
-      analysis_interval = seq(start_year, stop_year)
+  if( any(is.na(sightings)) ) return(NA)
 
-      if( length(s) < 2) return(analysis_interval*0)  # Not enough observations
+  analysis_interval = seq(start_year, stop_year)
 
-      as.vector(Vectorize(
-        function(t) {
-          if (t <= max(s)) return(1)
-          compute_posterior_solow1993(
-            sightings = s,
-            start_year = start_year,
-            end_year = t,
-            dprior_m = function(m) m ,
-            dprior_te =  level_prior_te,
-            prior = 0.5
-          )
-        }, SIMPLIFY = FALSE
-      )(analysis_interval))
+  if( length(sightings) < 2) return(analysis_interval*0)  # Not enough observations
+
+  if(is.null(prior_te)) {
+    prior_te = function(te) 1
+  }
+
+  if(is.null(prior_m)) {
+    prior_m = function(m) 1/m
+  }
+
+  Vectorize(
+    function(t) {
+      if (t <= max(sightings)) return(1)
+      compute_posterior_solow1993(
+        sightings = sightings,
+        start_year = start_year,
+        end_year = t,
+        dprior_m = prior_m,
+        dprior_te =  function(te) prior_te(te*(t-start_year)+start_year),
+        prior = 0.5
+      )
     }
-  )
+  )(analysis_interval)
 }
