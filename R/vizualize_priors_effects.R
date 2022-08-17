57c69d24f9111ebbee927ffba1a3380586aca90b
Clara-Casabona <claracasabona@gmail.com>
2022-08-17 12:00:35 -0400

    adding priors plots

--- R/vizualize_priors_effects.R
+++ R/vizualize_priors_effects.R
@@ -18,34 +18,59 @@ vizualize_priors_effects = function(t_start, t_stop, sightings, prior_te, prior_
 
   years = t_start:t_stop
   prior_te_eval = prior_te(years)
-  df <- melt(prior_te_eval)
+  df <- data.frame(prior_te_eval)
   df$year = years
 
+  seq_m = seq(0,10,by=0.01)
+  prior_m_eval = prior_m(seq_m)
+  dfm <- data.frame(prior_m_eval)
+  dfm$x = seq_m
+
   posterior_N2 = data.frame(posterior_N2)
-  names(posterior_N2)[1] = paste("Only_sightings")
+  names(posterior_N2)[1] = paste("Exant_posterior_NonInformativePriors")
   posterior_N2$year = years
 
   posterior_N2_prior_te_m = data.frame(posterior_N2_prior_te_m)
-  names(posterior_N2_prior_te_m)[1] = paste("Te_m_priors")
+  names(posterior_N2_prior_te_m)[1] = paste("Exant_posterior_Informative_m_te")
   posterior_N2_prior_te_m$year = seq(t_start, t_stop, by = 1)
 
   posterior_N2_prior_te = data.frame(posterior_N2_prior_te)
-  names(posterior_N2_prior_te)[1] = paste("Te_prior")
+  names(posterior_N2_prior_te)[1] = paste("Exant_posterior_Informative_te")
   posterior_N2_prior_te$year = seq(t_start, t_stop, by = 1)
 
   posterior_N2_prior_m = data.frame(posterior_N2_prior_m)
-  names(posterior_N2_prior_m)[1] = paste("m_priors")
+  names(posterior_N2_prior_m)[1] = paste("Exant_posterior_Informative_m")
   posterior_N2_prior_m$year = seq(t_start, t_stop, by = 1)
 
   New_df = full_join(posterior_N2,posterior_N2_prior_te_m)
   New_df = full_join(New_df,posterior_N2_prior_te)
   New_df = full_join(New_df,posterior_N2_prior_m)
-  New_df = tidyr::gather(New_df, "posterior", "value", c("Only_sightings","Te_prior","m_priors","Te_m_priors"))
+  New_df = tidyr::gather(New_df, "posterior", "value", c("Exant_posterior_NonInformativePriors","Exant_posterior_Informative_te","Exant_posterior_Informative_m","Exant_posterior_Informative_m_te"))
   New_df$posterior = as.factor(New_df$posterior)
-  ggplot(data=New_df, aes(x=year, y=value, group=posterior)) +
+
+  a=  ggplot(data=New_df, aes(x=year, y=value, group=posterior)) +
     theme_bw() +
     geom_line(aes(color=posterior)) +
-    scale_linetype_manual(values=c("dotted", "dotted","dotted","dotted")) +
-    scale_color_manual(values=c("yellow", "gray", "green", "blue")) +
+    # scale_linetype_manual(values=c("twodash","solid" ,"solid","twodash")) +
+    scale_color_manual(values=c("#00bbd6", "#237194","#faa32b", "gray")) +
     ylab("Extant probability")
-}
+
+  b =ggplot(dfm, aes(x=x, y=prior_m_eval)) +
+    theme_bw() +
+    theme(axis.title.x=element_text(size=8))+
+    geom_line() +
+    labs(x = "Observation rate before extinction (Prior m)", y = "Probability")
+
+c =  ggplot(df, aes(x=years, y=prior_te_eval)) +
+    theme_bw() +
+    theme(axis.title.x=element_text(size=8))+
+    geom_line() +
+    labs(x = "Posterior date of extinction probability  (Prior te)", y = "Probability")
+library(ggpubr)
+
+ ggarrange(a,                                                 # First row with scatter plot
+          ggarrange(c, b, ncol = 2, labels = c("B", "C"), widths = c(1,0.83)), # Second row with box and dot plots
+          nrow = 2,
+          labels = "A"                                        # Labels of the scatter plot
+)
+  }
