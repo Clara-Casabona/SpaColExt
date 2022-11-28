bf592e6b302460ce6e950557a87f5904af2ba5a1
Clara-Casabona <claracasabona@gmail.com>
2022-11-28 16:33:54 -0500

    add .Rd of mulitple functions

---
+++ R/Extant_probability_by_yearsnonhomogeneous.R
@@ -0,0 +1,33 @@
+#' Extant Posterior Probability sequence by years folowing non homogeneous
+#'
+#' @param start_year  Year at which we want to estimate the extant posterior probability
+#' @param stop_year   Year at which we stop to estimate the extant  posterior probability
+#' @param sightings   Vector with the years with  observations
+#'
+#' @return Vector with the extant probabilities
+#' @export
+#'
+#' @examples
+#'
+Posterior_probability_extinction_non_homogeneos_varying_end_year = function(sightings, start_year, stop_year) {
+
+  if( any(is.na(sightings)) ) return(NA)
+
+  analysis_interval = seq(start_year, stop_year)
+
+  if( length(sightings) < 2) return(analysis_interval = NA)  # Not enough observations
+
+  Vectorize(
+    function(t) {
+      if (t <= max(sightings)) return(1)
+      compute_posterior_non_homogeneous(
+        sightings = sightings,
+        start_year = start_year,
+        end_year = t,
+        dprior_m = solowdprior_m,
+        dprior_te = solowdprior_te,
+        prior = 0.5
+      )
+    }
+  )(analysis_interval)
+}
